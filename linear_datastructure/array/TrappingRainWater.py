eb7c342c764c6da87f418d1f92689d3dc74c9d44
sybarits <sybarits@gmail.com>
2022-02-14 00:08:06 +0900

    two pointer & stack
    두개의 포인터를 이용해 연산 하거나
    스택을 이용해 연산하거나..
    완전 어렵다.

--- linear_datastructure/array/TrappingRainWater.py
+++ linear_datastructure/array/TrappingRainWater.py
@@ -1,3 +1,4 @@
+# 높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라.
 class TrappingRainWater:
 
     def twoPointer(self, height: list[int]) -> int:
@@ -16,12 +17,28 @@ class TrappingRainWater:
 
         return volume
 
+    # 와... 너무 어렵다... 이거 다시 풀어보라고 하면 풀 수 있을까...
     def stackPop(self, height: list[int]) -> int:
-        pass
-        # 스택을 이용한 문제는 내공을 더 쌓고 도전하겠다.
-            
+        stack = []
+        total_volume = 0
+
+        for i, curH in enumerate(height):
+            while stack and curH > height[stack[-1]]:
+                top = stack.pop()
+                if not len(stack):
+                    break
+                waters = min(height[stack[-1]], curH) - height[top]
+                # 이미 처리한 물을 제외하기 위해 height[top]를 빼준다.
+                # min을 하는 이유는 둘중에 낮은 것을 기준으로 물 수위가 결정 되기 때문에
+                water_volume = waters * (i - stack[-1] - 1)
+                # i - stack[-1] - 1 은 얼마나 떨어진 블럭을 기준으로 할 건지에 대한 값이다.
+                total_volume += water_volume
+            stack.append(i)
+
+        return total_volume
 
 
 if __name__ == "__main__":
     trw = TrappingRainWater()
     print("volume is", trw.twoPointer([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(trw.stackPop([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
